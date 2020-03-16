e25f2b1b6ac8398c8d519874eb3fc979a57c82e6
David <chen7david@me.com>
2020-03-16 17:51:39 +0800

    update

--- schemas/objection.js
+++ schemas/objection.js
@@ -2,7 +2,10 @@ module.exports = (SchemaBuilder) => {
 
     const schema = new SchemaBuilder()
 
-    // define schemas here ...
+    schema.create('UniqueViolationError', {
+        en: (noun) => `${noun} is already in use!`, 
+        zh: (noun) => `${noun}已被占用`,
+    })
 
     return schema
 
